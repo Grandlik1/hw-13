c589d89758282f0fe6508f3bad94d1389471345a
Nikita <melnik.nikita27@gmail.com>
2022-04-27 00:48:14 +0300

    .

---
+++ js/task.js
@@ -0,0 +1,32 @@
+// Task1
+// let i;
+// for (i = 1; i < 100; i++) {
+//     console.log(i);
+// };
+
+// Task2
+// let i1;
+// for (i1 = 100; i1 > 1; i1--) {
+//     console.log(i1);
+// };
+
+// Task3
+// let i2;
+// for (i2 = 0; i2 <= 100; i2 += 2) {
+//     console.log(i2)
+// };
+// Task4
+// let array = [1, 2, 3, 4, 5];
+// let result = 0;
+// for (let i = 0; i < array.length; i++) {
+//     result += array[i];
+// }
+// console.log(result);
+
+// Task5
+// let array = [1, 2, 3, 4, 5];
+// let result = 0;
+// for (let i = 0; i < array.length; i++) {
+//     result += array[i]**2;
+// }
+// console.log(result);
