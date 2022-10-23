2cc5ed12f58caba0c9733868edb2bfa084071925
Nikita <90572220+Grandlik1@users.noreply.github.com>
2022-10-23 18:08:32 +0300

    .

--- js/task.js
+++ js/task.js
@@ -1,20 +1,20 @@
 // Task1
 // let i;
-// for (i = 1; i < 100; i++) {
+// for (i = 1; i <= 100; i++) {
 //     console.log(i);
 // };
 
 // Task2
 // let i1;
-// for (i1 = 100; i1 > 1; i1--) {
+// for (i1 = 100; i1 >= 1; i1--) {
 //     console.log(i1);
-// };
+// }
 
 // Task3
 // let i2;
 // for (i2 = 0; i2 <= 100; i2 += 2) {
 //     console.log(i2)
-// };
+// }
 // Task4
 // let array = [1, 2, 3, 4, 5];
 // let result = 0;
